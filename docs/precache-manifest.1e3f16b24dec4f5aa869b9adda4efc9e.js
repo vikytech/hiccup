26ccd3235222464030c009407d0b5918c7f77a82
Vikhyath <vikhyatc@thoughtworks.com>
2020-07-01 22:07:01 +0530

    Updated docs

---
+++ docs/precache-manifest.1e3f16b24dec4f5aa869b9adda4efc9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb405e65af9190f5726808ea13a561f",
+    "url": "/hiccup/index.html"
+  },
+  {
+    "revision": "c72bcea4afff3915a229",
+    "url": "/hiccup/static/css/main.df5ffb09.chunk.css"
+  },
+  {
+    "revision": "e74835cdf0b4817aebcc",
+    "url": "/hiccup/static/js/2.d218113e.chunk.js"
+  },
+  {
+    "revision": "c72bcea4afff3915a229",
+    "url": "/hiccup/static/js/main.48af3fc3.chunk.js"
+  },
+  {
+    "revision": "079820e82de307486ba8",
+    "url": "/hiccup/static/js/runtime~main.e5cbff15.js"
+  },
+  {
+    "revision": "c8ffdeb3144d5055756ef01ef98e8486",
+    "url": "/hiccup/static/media/OpenSans-Regular-webfont.c8ffdeb3.woff"
+  },
+  {
+    "revision": "03abf6a1eba045f805483a2ffdc4da00",
+    "url": "/hiccup/static/media/dot.03abf6a1.svg"
+  },
+  {
+    "revision": "89ffae1c492fc2d42286ad0a1fbac23f",
+    "url": "/hiccup/static/media/joker.89ffae1c.jpg"
+  },
+  {
+    "revision": "94d2a33e75aa77811a4d109012968314",
+    "url": "/hiccup/static/media/victory2.94d2a33e.gif"
+  }
+]);
