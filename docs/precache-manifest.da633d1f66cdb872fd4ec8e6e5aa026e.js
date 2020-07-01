e36994af1e29085bfd94c65204f85b7d5fb7267a
Vikhyath <vikhyatc@thoughtworks.com>
2020-07-01 22:17:23 +0530

    Updated docs

---
+++ docs/precache-manifest.da633d1f66cdb872fd4ec8e6e5aa026e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c0bb06d7c221c22508941e30cd9841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7d29bb58378805e2d6",
+    "url": "/static/css/main.5da43dd0.chunk.css"
+  },
+  {
+    "revision": "e74835cdf0b4817aebcc",
+    "url": "/static/js/2.d218113e.chunk.js"
+  },
+  {
+    "revision": "fe7d29bb58378805e2d6",
+    "url": "/static/js/main.48af3fc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8ffdeb3144d5055756ef01ef98e8486",
+    "url": "/static/media/OpenSans-Regular-webfont.c8ffdeb3.woff"
+  },
+  {
+    "revision": "03abf6a1eba045f805483a2ffdc4da00",
+    "url": "/static/media/dot.03abf6a1.svg"
+  },
+  {
+    "revision": "89ffae1c492fc2d42286ad0a1fbac23f",
+    "url": "/static/media/joker.89ffae1c.jpg"
+  },
+  {
+    "revision": "94d2a33e75aa77811a4d109012968314",
+    "url": "/static/media/victory2.94d2a33e.gif"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hiccup/precache-manifest.1e3f16b24dec4f5aa869b9adda4efc9e.js"
+  "/precache-manifest.da633d1f66cdb872fd4ec8e6e5aa026e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hiccup/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
